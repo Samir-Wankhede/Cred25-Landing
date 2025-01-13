3b6517592a81f74b041fe7ffbbd9d8f2c279c905
Samir Wankhede <samirsw04@gmail.com>
2025-01-13 15:28:04 +0530

    Dragon loader text added 9

--- src/components/3DComponents/Loader/DragonLoader.jsx
+++ src/components/3DComponents/Loader/DragonLoader.jsx
@@ -1,11 +1,15 @@
+import { Html } from "@react-three/drei";
+
 const DragonLoader = ({ loaded }) => {
-    if (!loaded) return null;
-    
-    return (
-      <div className="fixed top-4 left-4 text-white text-sm pointer-events-none z-50">
+  if (!loaded) return null;
+  
+  return (
+    <Html position={[0, 0, 0]} style={{ height: '20vh', top: 0, width: '100%' }} prepend>
+      <div className="text-white text-sm pointer-events-none pl-4 pt-4">
         Loading Dragon...
       </div>
-    );
-  };
-  
-  export default DragonLoader;
+    </Html>
+  );
+};
+
+export default DragonLoader;
