56543a927d3412c0acf97827e93b1065f78f1143
Samir Wankhede <samirsw04@gmail.com>
2025-01-13 15:32:08 +0530

    Dragon loader text added 10

--- src/components/3DComponents/Loader/DragonLoader.jsx
+++ src/components/3DComponents/Loader/DragonLoader.jsx
@@ -4,7 +4,7 @@ const DragonLoader = ({ loaded }) => {
   if (!loaded) return null;
   
   return (
-    <Html position={[0, 0, 0]} style={{ height: '20vh', top: 0, width: '100%' }} prepend>
+    <Html className="h-[20vh] w-full absolute" center={false} prepend>
       <div className="text-white text-sm pointer-events-none pl-4 pt-4">
         Loading Dragon...
       </div>
