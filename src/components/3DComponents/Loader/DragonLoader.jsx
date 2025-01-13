065fe1570a34ce360abe720ea3380bd2ca441918
Samir Wankhede <samirsw04@gmail.com>
2025-01-13 15:01:33 +0530

    Dragon loader text added 4

--- src/components/3DComponents/Loader/DragonLoader.jsx
+++ src/components/3DComponents/Loader/DragonLoader.jsx
@@ -3,11 +3,9 @@ import { Html, useProgress } from "@react-three/drei";
 const DragonLoader = ({loaded}) => {
   const { progress } = useProgress();
   return ( loaded &&
-    <Html center>
-      <div className="h-screen w-screen flex flex-col justify-start items-start relative">
-        <p className="text-sm custom-shadow2 text-white"> Loading Dragon {Math.floor(progress)}%</p>
-      </div>
-    </Html>
+
+    <p className="text-sm text-white absolute top-1 left-1"> Loading Dragon {Math.floor(progress)}%</p>
+
   );
 };
 
