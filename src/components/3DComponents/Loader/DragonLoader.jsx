f190107bf3a710db4209416aa8c2291fb4f31659
Samir Wankhede <samirsw04@gmail.com>
2025-01-13 15:11:59 +0530

    Dragon loader text added 7

--- src/components/3DComponents/Loader/DragonLoader.jsx
+++ src/components/3DComponents/Loader/DragonLoader.jsx
@@ -4,7 +4,7 @@ const DragonLoader = ({ loaded }) => {
   return (
     loaded && (
       <Html fullscreen>
-        <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none">
+        <div className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none">
           <p className="text-sm text-white absolute z-20 top-1 left-1 pointer-events-auto">
             Loading Dragon...
           </p>
