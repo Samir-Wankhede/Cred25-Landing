27990ccb3f0523c78093767f2cea66188268b3a0
Samir Wankhede <samirsw04@gmail.com>
2025-01-14 20:20:01 +0530

    main changes

--- src/components/3DComponents/Portal/Portal.jsx
+++ src/components/3DComponents/Portal/Portal.jsx
@@ -1,11 +1,10 @@
 import { useGLTF } from '@react-three/drei';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import FireParticles from '../Fire/fire';
 import  { Galaxy } from './NetherPortalParticles';
 
 const Portal = () => {
   const { scene } = useGLTF('/Portal.glb');
-  const ref = useRef();
   useEffect(()=>{
     scene.traverse((child)=>{
       child.frustumCulled = false;
@@ -18,7 +17,7 @@ const Portal = () => {
   },[scene])
   return (
     <>
-    <primitive object={scene} ref={ref} scale={0.3} rotation={[0,-Math.PI/2,0]} position={[0.75,-2.68,-1]} />
+    <primitive object={scene} scale={0.3} rotation={[0,-Math.PI/2,0]} position={[0.75,-2.68,-1]} />
     <Galaxy portal_pos={[0,-0.5,0.1]} mesh_pos={[0, -0.5, 0]} light_pos={[0,-1,1]}/>  
     <Galaxy portal_pos={[0,-0.5,-1.7]} mesh_pos={[0, -0.5, -1.5]} light_pos={[0,-1,-2]} mesh_rotate={Math.PI}/>  
     <FireParticles/>
