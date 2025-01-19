5dea5caf9aff587b303b9ad819ad5bbab5e03921
Samir Wankhede <samirsw04@gmail.com>
2025-01-19 21:42:34 +0530

    new bg for test

--- src/components/3DComponents/Nether/Nether.jsx
+++ src/components/3DComponents/Nether/Nether.jsx
@@ -2,7 +2,7 @@ import { useGLTF } from '@react-three/drei';
 import React, { useEffect } from 'react'
 
 const Nether = () => {
-  const { scene } = useGLTF('/Nether3.glb');
+  const { scene } = useGLTF('/Nether4.glb');
   useEffect(()=>{
     scene.traverse((child)=>{
       child.frustumCulled = false;
@@ -14,7 +14,7 @@ const Nether = () => {
     })
   },[scene])
   return (
-    <primitive object={scene} scale={0.3} rotation={[0,-Math.PI/4,0]} position={[0.75,-2.68, 0]} />
+    <primitive object={scene} scale={25} rotation={[0,-Math.PI/4,0]} position={[0.75,-2.68, 0]} />
   )
 }
 
